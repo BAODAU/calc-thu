8469358b6d6cf3d8cadf257364b82275b6604a03
DeeWeeBee <hi@dauquocbao.me>
2017-06-18 22:50:45 +0700

    Practicing

--- bot.js
+++ bot.js
@@ -1,8 +1,35 @@
-var botBuilder = require('claudia-bot-builder'),
-    excuse = require('huh');
+/*
+const botBuilder = require('claudia-bot-builder');
+const fbTemplate = require('claudia-bot-builder').fbTemplate;
 
-module.exports = botBuilder(function (request) {
-  return 'Thanks for sending ' + request.text  + 
-      '. Your message is very important to us, but ' + 
-      excuse.get();
+module.exports = botBuilder(message => {
+    if (message.type === 'facebook') {
+	const newMessage = new fbTemplate.Text('What\'s your favourite equation?');
+	return newMessage
+	    .addQuickReply('The integration one', INTEGRATION)
+      .addQuickReply('The slopy and trippy one xD', CURVE)
+      .addQuickReply('Anything', INCONCLUSIVE)
+      .get();
+    }
+  });
+*/
+
+const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
+
+module.exports = botBuilder(message => {
+  if (message.type === 'facebook') {
+    const generic = new fbTemplate.Generic();
+
+    return generic
+      .addBubble('Claudia.js', 'Deploy Node.js microservices to AWS easily')
+        .addUrl('https://claudiajs.com')
+        .addImage('https://claudiajs.com/assets/claudiajs.png')
+        .addButton('Say hello', 'HELLO')
+        .addButton('Go to Github', 'https://github.com/claudiajs/claudia')
+      .addBubble('Claudia Bot Builder')
+      	.addImage('https://claudiajs.com/assets/claudia-bot-builder-video.jpg')
+      	.addButton('Go to Github', 'https://github.com/claudiajs/claudia-bot-builder')
+      .get();
+  }
 });
