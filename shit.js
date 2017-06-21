d197210b0bc7fbbc533620efc7ed50e97e3b5432
DeeWeeBee <hi@dauquocbao.me>
2017-06-21 11:35:02 +0700

    remove shit.js

--- shit.js
+++
@@ -1,6 +0,0 @@
-var request = require('request');
-request('https://api.wolframalpha.com/v2/query?input=integrate+e+to+the+power+of+x+with+respect+to+x&appid=LY3LHG-K4QXJ737HJ',function(error,response,body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-});
