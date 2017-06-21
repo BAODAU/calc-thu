d11c80f0c329322e70fc9f06b408c21fd9aff9b9
DeeWeeBee <hi@dauquocbao.me>
2017-06-21 18:09:18 +0700

    asdf

--- wolfram-alpha/math/queryWolfram.js
+++ wolfram-alpha/math/queryWolfram.js
@@ -1,19 +1,4 @@
-'use strict'
-
-const rp = require('minimal-request-promise')
-const fbTemplate = require('claudia-bot-builder').fbTemplate
-const parseString = require('xml2js').parseString
-
-var fs = require('fs'),
-    xml2js = require 'xml2js'
-
-function askForWeather()  {
-    return rp.get('https://api.wolframalpha.com/v2/query?input=weather&appid=LY3LHG-K4QXJ737HJ&async=true')
-	.then(response => {
-	    const answer = parseString(response) })};
-
-parser = new xml2js.Parser()
-fs.readFile answer, (err, data) ->
-    parser.parseString data, (err, result) ->
-    console.dir result
-console.log
+var wolfram = require('wolfram-alpha').createClient("DEMO", opts);
+ 
+var results = yield wolfram.query("integrate 2x")
+console.log("Result: %j", results);
