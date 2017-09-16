6d626284cf400165ba2e923d740b6b5a4a6212d8
Kanishk Jain <kanotrix@pal-nat186-147-208.itap.purdue.edu>
2017-09-16 17:17:32 -0400

    I have added all the english comments in here

--- messenger/mainMenu.js
+++ messenger/mainMenu.js
@@ -4,10 +4,10 @@ const botBuilder  = require('claudia-bot-builder');
 const fbTemplate = botBuilder.fbTemplate;
 
 function mainMenu() {
-    return new fbTemplate.Button('Đây là những gì bạn làm được')
-	.addButton('Hướng dẫn sử dụng', 'EXAMPLE-BUTTON')
-  .addButton('Nhập phương trình', 'QUERY-BUTTON')
-	.addButton('Xem mã nguồn', 'CODE-BUTTON')
+    return new fbTemplate.Button('Đây là những gì bạn làm được') //this is what you do
+	.addButton('Manual instruction', 'EXAMPLE-BUTTON') //Manual instruction
+  .addButton('Please enter equation', 'QUERY-BUTTON') //Enter equation
+	.addButton('View the source code', 'CODE-BUTTON') // shows the source code
 	.get()
 }
 
