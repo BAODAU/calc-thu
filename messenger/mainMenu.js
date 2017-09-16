e196c05aa7d65e9328ce71aa74f744f830909f88
Kanishk Jain <kanotrix@pal-nat186-147-208.itap.purdue.edu>
2017-09-16 17:24:32 -0400

    asd

--- messenger/mainMenu.js
+++ messenger/mainMenu.js
@@ -4,7 +4,7 @@ const botBuilder  = require('claudia-bot-builder');
 const fbTemplate = botBuilder.fbTemplate;
 
 function mainMenu() {
-    return new fbTemplate.Button('Đây là những gì bạn làm được') //this is what you do
+    return new fbTemplate.Button('Choose from the following options:') //this is what you do
 	.addButton('Manual instruction', 'EXAMPLE-BUTTON') //Manual instruction
   .addButton('Please enter equation', 'QUERY-BUTTON') //Enter equation
 	.addButton('View the source code', 'CODE-BUTTON') // shows the source code
